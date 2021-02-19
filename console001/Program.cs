132be4a5964174efb19275a991b5bfeb1f8b492b
kusa-mochi <whoatemyapplepie@gmail.com>
2021-02-19 16:43:26 +0900

    .NETの練習用の環境を１つつくった。

---
+++ console001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
